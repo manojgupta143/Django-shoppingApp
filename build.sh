324ad95b34d781c2a4bd04bcc1da3489f0658d56
manoj gupta <mannusl4u@gmail.com>
2026-05-01 16:01:22 +0530

    Pin setuptools and wheel, and force reinstall them during build to fix pkg_resources

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python -m pip install --upgrade pip setuptools wheel
-python -m pip install --upgrade --force-reinstall setuptools
+python -m pip install --upgrade pip setuptools==82.0.0 wheel==0.42.0
+python -m pip install --force-reinstall setuptools==82.0.0 wheel==0.42.0
 python -m pip install --force-reinstall -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
