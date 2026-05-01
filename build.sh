dfab8093196f415ed0527d338e277ab18436ae4d
manoj gupta <mannusl4u@gmail.com>
2026-05-01 15:57:14 +0530

    Force reinstall setuptools and requirements during build to fix pkg_resources at runtime

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 set -o errexit
 
 python -m pip install --upgrade pip setuptools wheel
-python -m pip install -r requirements.txt
+python -m pip install --upgrade --force-reinstall setuptools
+python -m pip install --force-reinstall -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
