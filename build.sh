5129da12656edd67fc9e7ff91ee664e55705a0b4
manoj gupta <mannusl4u@gmail.com>
2026-05-01 15:53:47 +0530

    Use python -m pip for requirements install to ensure setuptools is available to the runtime

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 set -o errexit
 
 python -m pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
